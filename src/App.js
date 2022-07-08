76eb9afc96045e6392ffa2b4faecc31132dafa2d
Irina <thread8path@gmail.com>
2022-07-08 22:39:37 +0300

    created some components and elements

--- src/App.js
+++ src/App.js
@@ -1,17 +1,53 @@
-import logo from './logo.svg';
+import {Component} from 'react';
 import './App.css';
+
+const Header = () => {
+  return <h2>This is ReceiptsBook</h2>
+}
+
+/* const Field = () => {
+  const holder = 'Add a receipt';
+  const styledField = {
+    width: '300px'
+  };
+  return <input
+            placeholder={holder}
+            type="text"
+            style={styledField}/>
+} */
+
+class Field extends Component {
+  render() {
+    const holder = 'Add a receipt';
+    const styledField = {
+      width: '300px'
+    };
+
+    return <input
+    placeholder={holder}
+    type="text"
+    style={styledField}/>
+  }
+}
+
+function Btn() {
+  const text = 'Submit';
+  const wasSubmitted = false;
+/*   const res = () => {
+    return 'Submit';
+  } */
+//  const p = <p>Submit</p>
+  return <button>{wasSubmitted ? 'Thank you' : text}</button>
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <Header/>
+      <Field/>
+      <Btn/>
     </div>
   );
 }
-
+export {Header};
 export default App;
--- src/index.js
+++ src/index.js
@@ -9,13 +9,36 @@ import './index.css';
 import App from './App';
 // 5string measuring perfomance in the app измерение производительности
 // import reportWebVitals from './reportWebVitals';
-
+import {Header} from './App';
 // using the API
-// reactdom creates root node of the app, receiving selector, 
+
+// reactdom creates root node of the app, receiving selector, (where to be rendered) 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+//creating reactelements (variables) in jsx
+//const elem = <h2>Receipts to be ready</h2>; //or without ;, webpack solves if the ; needed
+//multistring - use () brackets
+//using variable
+//const text = 'ReceiptBook';
+/* const elem = (
+  //elem has a rootparent
+  //в фигурные скобки можно вставить все и массив - все преобразуется в строку сконкатенируется, нельзя объекты - ошибка
+  <div>
+    <h2 className='main-text'>This is {text}</h2>
+    <h2>More than {10 * 10} to be cooked</h2>
+    <h2>Receipts to be ready</h2>
+    <label htmlFor=""></label>
+    <input type="text" />
+    <button tabIndex={0}>Random Receipt</button>
+  </div>
+); */
+//creating elements variables without jsx (arguments - what element, class of element object classname, content of element) метод класса
+//const elem = React.createElement('h2', {className: 'tagline'}, 'Receipts to be ready');
 // to render something
 root.render(
+//(what is rendered)
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  //elem,
 );
